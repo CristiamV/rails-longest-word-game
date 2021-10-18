7e3217b1f7e6162eb6a664102d55b5ee7ff4d099
Cristiam Vasquez <cristiamvasquez@gmail.com>
2021-10-18 16:30:41 -0400

    2da actividad rails completado menos el opcional de colocar el score

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,36 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times { @letters << ('A'..'Z').to_a.sample }
+  end
+
+  def score    
+    @attemp = params[:word]
+    @letters = params[:letters].split(" ")
+    @valid_grid_word = valid_grid_word?(@attemp, @letters)
+    @valid_english_word = valid_english_word?(@attemp)
+  end
+
+  private
+  def valid_grid_word?(attemp, letters)
+    new_grid = letters.join.downcase.chars
+    attemp.chars.all? do |letter|
+      if new_grid.include?(letter)
+        new_grid.delete_at(new_grid.index(letter))
+        true
+      else
+        false
+      end
+    end
+  end
+
+  def valid_english_word?(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt.downcase}"
+    word_serialized = URI.open(url).read
+    word = JSON.parse(word_serialized)
+    word["found"]
+  end
+end
